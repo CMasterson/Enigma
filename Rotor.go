696aaa41b922328e2a1bf1b2eec6803ca36adc63
Conor Masterson <conor.masterson@bellmedia.ca>
2019-03-01 10:19:06 -0500

    RotorStack implemented, tested as working

--- Rotor.go
+++ Rotor.go
@@ -9,16 +9,19 @@ III	| BDFHJLCPRTXVZNYEIWGAKMUSQO |	  V	    |
   V	| VZBRGITYUPSDNHLXAWMJQOFECK |	  Z	    |
 */
 
+ var rotorSequences = [3][26]int{ 
+ 	[26]int{16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9, 4, 10, 12, 5, 11, 6, 3},
+    [26]int{18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4, 0, 9, 3, 10},
+    [26]int{16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9, 4, 10, 12, 5, 11, 6, 3},
+}
+
 type Rotor struct {
 	sequence [26]int
 	currentOffset int
 }
 
 func rotorForNumber(rotorNumber int, offset int) Rotor {
-	rotors := [][]int {{16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9, 4, 10, 12, 5, 11, 6, 3}, {18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4, 0, 9, 3, 10}}
-
-	return Rotor {rotors[rotorNumber], offset}
-
+	return Rotor {rotorSequences[rotorNumber], offset}
 }
 
 func (r Rotor) resultFromLeftIndex(leftIndex int) int {
--- RotorStack.go
+++ RotorStack.go
@@ -4,10 +4,24 @@ type RotorStack struct {
 	rotors [3]Rotor
 }
 
+func (rs *RotorStack) initStack(rotors [3]int, offsets [3]int) {
+	 for index := 0; index < 3; index++ {
+	 	rs.rotors[index] = rotorForNumber(rotors[index], offsets[index])
+	}
+}
+
 func (rs *RotorStack) incrementOffset() {
 	if rs.rotors[2].incrementOffset() {
 		if rs.rotors[1].incrementOffset() {
 			rs.rotors[0].incrementOffset()
 		}
 	}
+}
+
+func (rs RotorStack) encodeLeftIndex(index int) int {
+	return rs.rotors[2].resultFromLeftIndex(rs.rotors[1].resultFromLeftIndex(rs.rotors[0].resultFromLeftIndex(index)))
+}
+
+func (rs RotorStack) encodeRightIndex(index int) int {
+	return rs.rotors[0].resultFromRightIndex(rs.rotors[1].resultFromRightIndex(rs.rotors[2].resultFromRightIndex(index)))
 }
